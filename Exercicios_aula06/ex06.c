3c1452a4d724744f12ec4073eb228f656020eddc
Carlinn01 <carloseduardodalazenoliveira@gmail.com>
2026-03-17 22:05:12 -0300

    Final Aula 17/03

---
+++ Exercicios_aula06/ex06.c
@@ -0,0 +1,17 @@
+float nota;
+scanf("%f", &nota);
+if (nota >= 9.0) {
+    printf("Excelente");
+}
+else if (nota >= 7.0)
+{
+    printf("Bom");
+}
+else if (nota >= 5.0)
+{
+    printf("Regular");
+}
+else {
+    printf("Reprovado");
+}
+
